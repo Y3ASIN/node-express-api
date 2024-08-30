ffc1944c7a86b68a8c44f14242efc9ce656a1325
Yeasin Arafat <yeasinarafat@Yeasins-Air.local>
2024-08-30 23:38:14 +0600

    Update: Basic CRUD with node & express.

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,14 @@
 import express from "express";
 
+import {getAllUsers,getAUser, createUser, updateUser, deleteUser } from "../controllers/users.js";
+
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 23,
-  },
-  {
-    firstName: "Petar",
-    lastName: "Parker",
-    age: 24,
-  },
-];
+router.get("/", getAllUsers);
+router.post("/", createUser);
 
-router.get("/", (req, res) => {
-  res.send(users);
-});
+router.get("/:id", getAUser);
+router.delete("/:id", deleteUser);
+router.patch("/:id", updateUser);
 
 export default router;
